585b6375994a4d71024b76d7c3dead34a4c81a8b
Kevin W Monroe <kevin.monroe@canonical.com>
2018-08-26 23:25:31 -0500

    bump to recent 3.2.1 release

--- build-calico-resource.sh
+++ build-calico-resource.sh
@@ -5,10 +5,12 @@ rm -rf resource-build
 mkdir resource-build
 cd resource-build
 # Please refer to layer-canal/versioning.md before changing any of these versions.
-wget https://github.com/projectcalico/calicoctl/releases/download/v1.6.4/calicoctl
-wget https://github.com/projectcalico/cni-plugin/releases/download/v1.11.6/calico
-wget https://github.com/projectcalico/cni-plugin/releases/download/v1.11.6/calico-ipam
+wget https://github.com/projectcalico/calicoctl/releases/download/v3.2.1/calicoctl
+wget https://github.com/projectcalico/cni-plugin/releases/download/v3.2.1/calico
+wget https://github.com/projectcalico/cni-plugin/releases/download/v3.2.1/calico-ipam
+
 chmod +x calicoctl calico calico-ipam
+
 tar -vcaf ../calico-resource.tar.gz .
 cd ..
 rm -rf resource-build
