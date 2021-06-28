efe1f38172492db81e571690ee8dcb38bb216a36
Cory Johns <johnsca@gmail.com>
2021-06-28 14:55:25 -0400

    Fix validate-wheelhouse

--- tests/validate-wheelhouse.sh
+++ tests/validate-wheelhouse.sh
@@ -5,4 +5,4 @@ function cleanup { rm -rf "$build_dir"; }
 trap cleanup EXIT
 
 charm build . --build-dir "$build_dir"
-pip install -f "$build_dir/calico/wheelhouse" --no-index --no-cache-dir "$build_dir"/calico/wheelhouse/*
+pip install -f "$build_dir/canal/wheelhouse" --no-index --no-cache-dir "$build_dir"/canal/wheelhouse/*
