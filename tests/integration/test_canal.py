4521ff24b79be97f323eb77f096f8dbd4d30d94f
Adam Dyess <adam.dyess@canonical.com>
2022-01-28 15:12:23 -0600

    inclusive naming for kubernetes-master (#66)
    * inclusive naming for kubernetes-master
    
    * Download resources from previous runs if the integration tests cannot build them
    
    * switch to VWMare testing
    
    * ensure all files referenced in the integration bundle use an arch reference
    
    * updated control-plane charm name

--- tests/integration/test_canal.py
+++ tests/integration/test_canal.py
@@ -1,30 +1,27 @@
 import logging
 import pytest
 from pathlib import Path
+import shlex
+
 
 log = logging.getLogger(__name__)
-
 CHARM_DIR = Path(__file__).parent.parent.parent
 RESOURCE_BUILD_SCRIPT = CHARM_DIR / "build-canal-resources.sh"
 BUNDLE_PATH = Path(__file__).parent / "bundle.yaml"
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test):
-    resource_path = ops_test.tmp_path / "charm-resources"
-    resource_path.mkdir()
-    log.info("Building charm resources")
-    await ops_test.run(
-        RESOURCE_BUILD_SCRIPT,
-        cwd=resource_path,
-        check=True,
-        fail_msg="Failed to build charm resources",
-    )
+async def test_build_and_deploy(ops_test, setup_resources):
+    log.info("Build Charm...")
+    charm = await ops_test.build_charm(".")
+
+    log.info("Build Bundle...")
     bundle = ops_test.render_bundle(
         "tests/data/bundle.yaml",
-        canal_charm=await ops_test.build_charm("."),
-        resource_path=resource_path,
+        canal_charm=charm,
+        resource_path=setup_resources,
     )
+
     # deploy with Juju CLI because libjuju does not support local resource
     # files (see https://github.com/juju/python-libjuju/issues/223)
     log.info("Deploying bundle")
@@ -38,19 +35,13 @@ async def test_build_and_deploy(ops_test):
         fail_msg="Failed to deploy bundle",
     )
     try:
-        await ops_test.model.wait_for_idle(wait_for_active=True, timeout=30 * 60)
+        await ops_test.model.wait_for_idle(wait_for_active=True, timeout=60 * 60)
     finally:
-        await ops_test.run(
-            "juju-crashdump",
-            "-s",
-            "-a",
-            "debug-layer",
-            "-a",
-            "config",
-            "-m",
-            ops_test.model_full_name,
-        )
-        unit = ops_test.model.applications["kubernetes-master"].units[0]
+        model = ops_test.model_full_name
+        cmd = f"juju-crashdump -s -a debug-layer -a config -m {model}"
+        await ops_test.run(*shlex.split(cmd))
+        k8s_cp = "kubernetes-control-plane"
+        unit = ops_test.model.applications[k8s_cp].units[0]
         response = await unit.run(
             "kubectl --kubeconfig /root/.kube/config get all -A", timeout=30
         )
