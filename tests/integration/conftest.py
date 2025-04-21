2aa5498588051e831965749b3b23ad230894338a
Adam Dyess <adam.dyess@canonical.com>
2025-04-21 15:25:24 -0500

    Drop focal add noble (#84)
    * Drop focal, build with charmcraft
    * Update py deps
    * Default the snaps to the most stable snaps

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,34 @@
+import re
+import subprocess
 import logging
+import packaging.version
 import pytest
 
 log = logging.getLogger(__name__)
+STABLE_CHANNEL = re.compile(r"^\s+(\d+\.\d+)\/stable:\s+(\S+)")
+
+
+def default_kubernetes_channel() -> str:
+    """
+    Get the default Kubernetes snap channel.
+    """
+    result = subprocess.run(
+        ["snap", "info", "kubelet"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    stables = [
+        match.group(1)
+        for line in result.stdout.splitlines()
+        if (match := STABLE_CHANNEL.match(line))  # numerical channel
+        and match.group(2) != "--"  # not an empty channel
+    ]
+    if not stables:
+        raise RuntimeError("No stable channel found for kubelet snap")
+    most_stable = sorted(stables, key=packaging.version.parse, reverse=True)[0]
+    log.debug(f"Most stable channel found: {most_stable}")
+    return f"{most_stable}/stable"
 
 
 def pytest_addoption(parser):
@@ -14,7 +41,7 @@ def pytest_addoption(parser):
     parser.addoption(
         "--snap-channel",
         type=str,
-        default="latest/stable",
+        default=default_kubernetes_channel(),
         help="Set snap channel for the control-plane & worker units",
     )
 
